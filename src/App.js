34fe4e9cded8acfdccdf649ee7668e5e1470d8c5
arfin-web <arfinpriom@gmail.com>
2022-02-09 16:47:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './Pages/Header/Header';
+import Home from './Pages/Home/Home';
+import Addproduct from './Pages/Addproduct/Addproduct';
+import Allproducts from './Pages/Allproducts/Allproducts';
+import Signin from './Pages/Signin/Signin';
+import Signup from './Pages/Signup/Signup';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+import Authprovider from './Context/Authprovider';
+import Purchase from './Pages/Purchase/Purchase';
+import Orders from './Pages/Orders/Orders';
+import Myorders from './Pages/Myorders/Myorders';
+import Makeadmin from './Pages/Makeadmin/Makeadmin';
+import Privateroute from './Privateroute/Privateroute';
+import Adminroute from './Adminroute/Adminroute';
+import Makereview from './Pages/Makereview/Makereview';
+import Footer from './Pages/Footer/Footer';
+import Manageproduct from './Pages/Manageproduct/Manageproduct';
+import Notfound from './Pages/Notfound/Notfound';
+import Pay from './Pages/Pay/Pay';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Authprovider>
+        <BrowserRouter>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            <Route path="/about">
+              <About></About>
+            </Route>
+            <Route path="/contact">
+              <Contact></Contact>
+            </Route>
+            <Route path="/signin">
+              <Signin></Signin>
+            </Route>
+            <Route path="/signup">
+              <Signup></Signup>
+            </Route>
+            <Route exact path="/toys">
+              <Allproducts></Allproducts>
+            </Route>
+            <Privateroute path="/toys/:id">
+              <Purchase></Purchase>
+            </Privateroute>
+            <Privateroute path="/myorders">
+              <Myorders></Myorders>
+            </Privateroute>
+            <Privateroute path="/makereviews">
+              <Makereview></Makereview>
+            </Privateroute>
+            <Privateroute path="/pay">
+              <Pay></Pay>
+            </Privateroute>
+            <Adminroute path="/makeadmin">
+              <Makeadmin></Makeadmin>
+            </Adminroute>
+            <Adminroute path="/addproduct">
+              <Addproduct></Addproduct>
+            </Adminroute>
+            <Adminroute path="/manageproduct">
+              <Manageproduct></Manageproduct>
+            </Adminroute>
+            <Adminroute path="/orders">
+              <Orders></Orders>
+            </Adminroute>
+            <Route path="*">
+              <Notfound></Notfound>
+            </Route>
+          </Switch>
+          <Footer></Footer>
+        </BrowserRouter>
+      </Authprovider>
+    </>
   );
 }
 
